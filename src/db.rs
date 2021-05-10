1f093def3e1e7ab97db6636fcaf372b6e4ea74a3
Doran Kayoumi <dorankayoumi@gmail.com>
2021-05-10 17:18:07 +0200

    Add function to update user in db & return created user

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,7 @@
 pub mod models;
 pub mod schema;
 
-use diesel::{insert_into, prelude::*, result::Error};
+use diesel::{insert_into, prelude::*, result::Error, update};
 use dotenv::dotenv;
 use std::env;
 
@@ -21,9 +21,18 @@ pub fn get_user(e: &str) -> Result<User, Error> {
     users.filter(email.eq(e)).first::<User>(&conn)
 }
 
-pub fn create_user(u: &NewUser) {
+pub fn create_user(u: &NewUser) -> Result<User, Error> {
     let conn = establish_connection();
     if let Err(e) = insert_into(users).values(u).execute(&conn) {
+        return Err(e);
+    }
+
+    Ok(get_user(u.email).unwrap())
+}
+
+pub fn update_user(u: &User) {
+    let conn = establish_connection();
+    if let Err(e) = update(users.filter(id.eq(u.id))).set(u).execute(&conn) {
         println!("{}", e);
     }
 }
