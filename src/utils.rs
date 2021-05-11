f9d77c02856547bc41f99952930b873139a7825a
Doran Kayoumi <dorankayoumi@gmail.com>
2021-05-11 15:44:34 +0200

    Add new utils function for password reset

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,6 @@
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use regex::{self, Regex};
 use sodiumoxide::crypto::pwhash::argon2id13;
 
 pub fn hash(passwd: &str) -> String {
@@ -18,4 +21,18 @@ pub fn verify_hash(passwd: &str, pwh: &str) -> bool {
 
     let hp = argon2id13::HashedPassword::from_slice(pwh.as_bytes()).unwrap();
     argon2id13::pwhash_verify(&hp, passwd.as_bytes())
+}
+
+pub fn gen_token() -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect()
+}
+
+pub fn check_cmd_syntax(s: &str) -> bool {
+    let re: Regex = Regex::new(r"^([A-Za-z]+)$|^(\d+)$").unwrap();
+
+    re.is_match(&s)
 }
