b705c8f248300d152b2e3ca62373351142713807
Doran Kayoumi <dorankayoumi@gmail.com>
2021-05-11 20:01:07 +0200

    Add tests for 2fa functions

--- src/auth/twofa.rs
+++ src/auth/twofa.rs
@@ -14,7 +14,6 @@ pub fn check_code(secret: &str, code: &str) -> bool {
 }
 
 /// Generates a secret for the 2fa
-///
 pub fn generate_secret() -> String {
     let auth = GoogleAuthenticator::new();
     auth.create_secret(32)
@@ -35,4 +34,39 @@ pub fn generate_secret() -> String {
 pub fn generate_qr(secret: &str, name: &str, title: &str) -> String {
     let auth = GoogleAuthenticator::new();
     auth.qr_code_url(secret, name, title, 400, 400, ErrorCorrectionLevel::High)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_check_code() {
+        let secret = "I3VFM3JKMNDJCDH5BMBEEQAW6KJ6NOE3";
+        let auth = GoogleAuthenticator::new();
+        let code = auth.get_code(secret, 0).unwrap();
+
+        assert_eq!(check_code(secret, &code), true);
+        assert_eq!(check_code(secret, "000000"), false);
+    }
+
+    #[test]
+    fn test_generate_secret() {
+        let secret = generate_secret();
+
+        assert_eq!(secret.len(), 32);
+    }
+
+    #[test]
+    fn test_generate_qr() {
+        let secret = "I3VFM3JKMNDJCDH5BMBEEQAW6KJ6NOE3";
+        let name = "test";
+        let title = "test";
+        let qr_url = generate_qr(secret, name, title);
+
+        assert_eq!(qr_url.contains(secret), true);
+        assert_eq!(qr_url.contains(name), true);
+        assert_eq!(qr_url.contains(title), true);
+        assert_eq!(qr_url.contains("http"), true);
+    }
 }
