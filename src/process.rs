a79d44aa65c479ff9658193a6c4db917b630009f
Doran Kayoumi <dorankayoumi@gmail.com>
2021-05-11 17:20:59 +0200

    Remove dups from process.rs and little refactors

--- src/process.rs
+++ src/process.rs
@@ -8,8 +8,8 @@ pub fn login_process() -> User {
     loop {
         let email = user_input::ask_for_email();
         let passwd = user_input::ask_for_password();
-        let u = login::login(&email, &passwd);
 
+        let u = login::login(&email, &passwd);
         if let Err(e) = u {
             println!("{}", e);
             continue;
@@ -31,8 +31,8 @@ pub fn registration_process() {
     loop {
         let email = user_input::ask_for_email();
         let passwd = user_input::ask_for_password_with_policy_check();
-        let u = register::register(&email, &passwd);
 
+        let u = register::register(&email, &passwd);
         if let Err(e) = u {
             println!("{}", e);
             continue;
@@ -49,7 +49,7 @@ pub fn reset_passwd_process() {
     println!("In case a user with that data exists in our database, you'll recieve the token to reset your password");
 
     if let Err(_) = reset::generate_reset_token(&email) {
-        // exit the process
+        // exit the process without informing the user to avoid anyforms of attacks
         return;
     }
 
@@ -60,16 +60,26 @@ pub fn reset_passwd_process() {
         return;
     }
 
+    // ideally all of the following would be handeled somewhere else
+    // and the `send_reset_token` would send an email with a url that hte user needs to click to follow th reset instructions
+
+    // get the user from the db
     let u = db::repository::get_user(&email);
-    if let Err(_) = u {
-        return;
+    if let Err(e) = u {
+        // something bad happened (e.g. the db is down)
+        // Note: The problem can't come from the non existance of the user
+        //       because `generate_reset_token` generates a token only if the user exists.
+        //       hence the panic.
+        panic!(e);
     }
     let u = u.unwrap();
 
-    println!("Confirm your identity:");
-    // we can safely get the users 2FA secret
-    let secret = u.get_secret_2fa().unwrap();
-    confirm_2fa_code(&secret);
+    if u.is_2fa_enabled() {
+        println!("Confirm your identity:");
+        // we can safely get the users 2FA secret
+        let secret = u.get_secret_2fa().unwrap();
+        confirm_2fa_code(&secret);
+    }
 
     let passwd = user_input::ask_for_password_with_policy_check();
     if let Err(e) = reset::change_password(&email, &passwd) {
@@ -88,11 +98,7 @@ pub fn enable_2fa_process(u: &mut User) {
     // Before adding the 2FA, confirm the users identity
     // by asking for hes/his password
     println!("Confirm your identity:");
-    let passwd = user_input::ask_for_password();
-    if !utils::verify_hash(&passwd, &u.get_password()) {
-        println!("Confirmation failed");
-        return;
-    }
+    confirm_identity_with_passwd(&u.get_password());
 
     // generate the 2FA secret & the QR code so the user can add the secret
     // to her/his 2FA authentication app
@@ -116,6 +122,34 @@ pub fn enable_2fa_process(u: &mut User) {
     };
 }
 
+pub fn disable_2fa_process(u: &mut User) {
+    // quick check that the user doesn't already have 2fa activated
+    // you never know...
+    if !u.is_2fa_enabled() {
+        println!("Two-factor authentication already disabled");
+        return;
+    }
+
+    // Before touching the 2FA, confirm the users identity
+    // by asking for hers/his password
+    println!("Confirm your identity:");
+    confirm_identity_with_passwd(&u.get_password());
+
+    // Ask the user to input a authentication code
+    // to confirm she/he correctly setup the 2FA
+    let secret = u.get_secret_2fa().unwrap(); // we can safely get the users 2FA secret
+    confirm_2fa_code(&secret);
+
+    // NOTE: For some reason this doesn't remove the secret from the DB
+    // TODO: Fix
+    // update the database with the changes
+    u.set_secret_2fa(None);
+    if let Err(_) = db::repository::update_user(&u) {
+        println!("Two-factor authentication failed.");
+        return;
+    };
+}
+
 fn confirm_2fa_code(secret: &str) {
     loop {
         let auth_code = user_input::ask_for_authentication_code();
@@ -127,38 +161,10 @@ fn confirm_2fa_code(secret: &str) {
     }
 }
 
-pub fn disable_2fa_process(u: &mut User) {
-    // quick check that the user doesn't already have 2fa activated
-    // you never know...
-    if !u.is_2fa_enabled() {
-        println!("Two-factor authentication already disabled");
-        return;
-    }
-
-    // STEP 1 ask for password & 2FA code
-    //
-    // Before touching the 2FA, confirm the users identity
-    // by asking for hers/his password
-    println!("Confirm your identity (1):");
+fn confirm_identity_with_passwd(user_passwd: &str) {
     let passwd = user_input::ask_for_password();
-    if !utils::verify_hash(&passwd, &u.get_password()) {
+    if !utils::verify_hash(&passwd, user_passwd) {
         println!("Two-factor authentication already enabled");
         return;
     }
-
-    // Ask the user to input a authentication code
-    // to confirm she/he correctly setup the 2FA
-    println!("Confirm your identity (2):");
-    // we can safely get the users 2FA secret
-    let secret = u.get_secret_2fa().unwrap();
-    confirm_2fa_code(&secret);
-
-    // NOTE: For some reason this doesn't remove the secret from the DB
-    // TODO: Fix
-    // update the database with the changes
-    u.set_secret_2fa(None);
-    if let Err(_) = db::repository::update_user(&u) {
-        println!("Two-factor authentication failed.");
-        return;
-    };
 }
