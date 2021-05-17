5ab63e8263f75f9106d21f58d6e58a1c26326ca2
Doran Kayoumi <dorankayoumi@gmail.com>
2021-05-17 17:37:13 +0200

    Remove test that doesnt work

--- src/auth/login.rs
+++ src/auth/login.rs
@@ -35,27 +35,3 @@ fn _login(email: &str, password: &str, repository: &dyn UserRepository) -> Resul
         Err(AuthError::LoginError)
     }
 }
-
-// #[cfg(test)]
-// mod test {
-//     use super::*;
-//     use crate::db::repository::MockSQliteUserRepository;
-
-//     #[test]
-//     fn mytest() {
-//         let mut mock = MockSQliteUserRepository::new();
-//         mock.expect_test().returning(|e| {
-//             Ok(User {
-//                 id: 1,
-//                 email: e.to_string(),
-//                 password: "$argon2id$v=19$m=65536,t=2,p=1$3fuwf7phG607snj/BveoNQ$0D2fU3kTj7EXPXEsw8vKOiNhRkoo3nPJaPC+451+/3M".to_string(),
-//                 secret_2fa: None,
-//                 reset_token: None,
-//                 reset_token_created_at: None,
-//             })
-//         });
-
-//         let u = _login("email", "password", &mock);
-//         println!("{:?}", u);
-//     }
-// }
