663c46f6d125eba2d80792f273c98f320eceb93a
Doran Kayoumi <dorankayoumi@gmail.com>
2021-05-17 17:35:32 +0200

    Create trait for User repository and refactor old one

--- src/db/repository.rs
+++ src/db/repository.rs
@@ -7,15 +7,15 @@ use super::schema::users::dsl::*;
 use crate::errors::UserDBError;
 
 pub trait UserRepository {
-    fn get_user(e: &str) -> Result<User, UserDBError>;
-    fn create_user(u: &NewUser) -> Result<(), UserDBError>;
-    fn update_user(u: &User) -> Result<(), UserDBError>;
+    fn get_user(&self, e: &str) -> Result<User, UserDBError>;
+    fn create_user(&self, e: &str, passwd: &str) -> Result<(), UserDBError>;
+    fn update_user(&self, u: &User) -> Result<(), UserDBError>;
 }
 
 pub struct SQliteUserRepository {}
 
 impl UserRepository for SQliteUserRepository {
-    fn get_user(e: &str) -> Result<User, UserDBError> {
+    fn get_user(&self, e: &str) -> Result<User, UserDBError> {
         let conn = establish_connection();
         let res = users.filter(email.eq(e)).first::<User>(&conn);
 
@@ -26,7 +26,12 @@ impl UserRepository for SQliteUserRepository {
         }
     }
 
-    fn create_user(u: &NewUser) -> Result<(), UserDBError> {
+    fn create_user(&self, e: &str, passwd: &str) -> Result<(), UserDBError> {
+        let u = NewUser {
+            email: e,
+            password: passwd,
+        };
+
         let conn = establish_connection();
         if let Err(_) = insert_into(users).values(u).execute(&conn) {
             return Err(UserDBError::CreateUserError);
@@ -35,7 +40,7 @@ impl UserRepository for SQliteUserRepository {
         Ok(())
     }
 
-    fn update_user(u: &User) -> Result<(), UserDBError> {
+    fn update_user(&self, u: &User) -> Result<(), UserDBError> {
         let conn = establish_connection();
         if let Err(_) = update(users.filter(id.eq(u.get_id())))
             .set(u)
